47a6bb46cef013b2feb4e5483dee2cd0dfc98978
dudezi <a01036952858@gmail.com>
2024-10-06 19:42:20 +0900

    init

---
+++ src/main/java/com/market/allForOneReview/AllForOneReviewApplication.java
@@ -0,0 +1,13 @@
+package com.market.allForOneReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllForOneReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllForOneReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/allForOneReview/AllForOneReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.allForOneReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllForOneReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
