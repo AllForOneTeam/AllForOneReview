081abc5b7b69e211a12648b1c278bbdbf3e1915f
jaehyuk7 <jahyuck13@naver.com>
2024-10-10 12:40:04 +0900

    Feature/6 (#7)
    * feat:맴버엔티티 1차 추가
    
    * feat:맴버엔티티 2차 추가

--- src/main/java/com/market/allForOneReview/domain/member/entity/Member.java
+++ src/main/java/com/market/allForOneReview/domain/member/entity/Member.java
@@ -14,7 +14,24 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member extends BaseEntity {
-    @Column(unique = true)
-    private String username;
+    @Column(length = 100, nullable = false, unique = true)
+    private String userId;
+
+    @Column(length = 100, nullable = false)
+    private String nickname;
+
+    @Column(name = "password", nullable = false, length = 100)
     private String password;
+
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
+
+    @Column
+    private int authority;
+
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+//    private List<ReviewPost> reviewPosts;
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+//    private List<NoticePost> noticePosts;
 }
