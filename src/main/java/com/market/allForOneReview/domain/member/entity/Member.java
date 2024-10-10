96a4b5a9c72d3c5c2d08a452a76e20e19b9f1c4c
jaehyuk7 <jahyuck13@naver.com>
2024-10-10 12:07:12 +0900

    feat:맴버엔티티 2차 추가

--- src/main/java/com/market/allForOneReview/domain/member/entity/Member.java
+++ src/main/java/com/market/allForOneReview/domain/member/entity/Member.java
@@ -1,14 +1,12 @@
 package com.market.allForOneReview.domain.member.entity;
 
 import com.market.allForOneReview.global.jpa.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedDate;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -16,23 +14,24 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(length = 100, nullable = false, unique = true)
+    private String userId;
 
-    @Column(unique = true)
-    private String username;
-
-    @Column(unique = true)
+    @Column(length = 100, nullable = false)
     private String nickname;
 
+    @Column(name = "password", nullable = false, length = 100)
     private String password;
 
-    @Column(unique = true)
+    @Column(name = "email", nullable = false, length = 100)
     private String email;
 
-    @CreatedDate
-    private LocalDateTime createdDate;
+    @Column
+    private int authority;
 
-    private Integer Authority;
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+//    private List<ReviewPost> reviewPosts;
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+//    private List<NoticePost> noticePosts;
 }
