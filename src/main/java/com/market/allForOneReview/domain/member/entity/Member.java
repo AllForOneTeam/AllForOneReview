744deae7a319760fa701fb7bc7560ece56ac0a16
jaehyuk7 <jahyuck13@naver.com>
2024-10-10 11:49:59 +0900

    feat:맴버엔티티 1차 추가

--- src/main/java/com/market/allForOneReview/domain/member/entity/Member.java
+++ src/main/java/com/market/allForOneReview/domain/member/entity/Member.java
@@ -1,12 +1,14 @@
 package com.market.allForOneReview.domain.member.entity;
 
 import com.market.allForOneReview.global.jpa.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -14,7 +16,23 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(unique = true)
     private String username;
+
+    @Column(unique = true)
+    private String nickname;
+
     private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    private Integer Authority;
 }
