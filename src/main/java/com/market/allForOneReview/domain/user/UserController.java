66e1ea54ca710006d32b55f1aa2f0ae33a23140c
jaehyuk7 <jahyuck13@naver.com>
2024-10-11 10:34:57 +0900

    feat:userController추가
    # Conflicts:
    #	src/main/java/com/market/allForOneReview/domain/user/UserController.java

--- src/main/java/com/market/allForOneReview/domain/user/UserController.java
+++ src/main/java/com/market/allForOneReview/domain/user/UserController.java
@@ -1,19 +1,39 @@
 package com.market.allForOneReview.domain.user;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm) {
+        return "signup_form";
     }
 
-    @GetMapping("/membership")
-    public String membership() {
-        return "membership";
-    }
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
 
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+
+        userService.create(userCreateForm.getUserId(), userCreateForm.getNickname(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+
+        return "redirect:/";
+    }
 }
