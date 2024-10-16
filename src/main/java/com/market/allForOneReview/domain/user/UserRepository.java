754cc155ed81decf30971517f11991ab325f3d45
jaehyuk7 <jahyuck13@naver.com>
2024-10-16 11:17:06 +0900

    feat:로그인 기능 추가

--- src/main/java/com/market/allForOneReview/domain/user/UserRepository.java
+++ src/main/java/com/market/allForOneReview/domain/user/UserRepository.java
@@ -3,8 +3,12 @@ package com.market.allForOneReview.domain.user;
 import com.market.allForOneReview.domain.user.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
     boolean existsByUserId(String userId);
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
+
+    Optional<SiteUser> findByUserId(String userId);
 }
